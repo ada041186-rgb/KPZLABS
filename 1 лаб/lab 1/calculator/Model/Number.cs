e294605dd89febda9fa7349df7c250832c2a1843
Кукіль Костянтин Павлович <ipz241_kkp@student.ztu.edu.ua>
2026-01-28 13:17:41 +0200

    Add project files.

---
+++ 1 лаб/lab 1/calculator/Model/Number.cs
@@ -0,0 +1,15 @@
+﻿using calculator.Enumes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Model
+{
+    public class Number
+    {
+        public double Value { get; set; }
+        public Operators Operation { get; set; } = Operators.None;
+    }
+}
---
+++ 1 лаб/lab 1/calculator/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace calculator.ViewModel
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        private static MainViewModel _instance;
+        public static MainViewModel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new MainViewModel();
+                }
+                return _instance;
+            }
+        }
+        public MainViewModel() 
+        {
+            CurrentViewModel = new BottonsViewModel();
+        }
+        private BaseViewModel   _currentViewModel;
+        public BaseViewModel CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged();
+
+            }
+        }
+        
+    }
+}
